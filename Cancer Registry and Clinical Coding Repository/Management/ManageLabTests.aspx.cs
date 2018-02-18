c4a273f06e530712620d2aa62aca92f86b3c6f55
chris-mcclain <ccpdrummer@gmail.com>
2018-02-17 21:02:46 -0500

    Update ManageLabTests.aspx.cs

--- Cancer Registry and Clinical Coding Repository/Management/ManageLabTests.aspx.cs
+++ Cancer Registry and Clinical Coding Repository/Management/ManageLabTests.aspx.cs
@@ -1,9 +1,7 @@
 ﻿/*
-  Class: CPDM Capstone
   Author: Chris McClain
   Abstract: Allows management to add, edit and delete
             lab tests in the Cancer Registry.
-  Date: July 25, 2017
 */
 using System;
 using System.Collections.Generic;
